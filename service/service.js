2f4995d197244ccc37a358df10e5dd848c8d98cc
007tickooayush <ayush.tickoo292000@gmail.com>
2021-07-27 09:47:53 +0530

    mongoose added, User model created

--- service/service.js
+++ service/service.js
@@ -1,23 +1,49 @@
 // static data
-const users = [
-    {
-        username: 'hellsent',
-        email: 'hellsent@gmail.com'
-    },
-    {
-        username: 'ayush',
-        email: 'ayush@gmail.com'
-    },
-    {
-        username: 'ayush1',
-        email: 'ayush1@gmail.com'
-    }
-];
+// let users = [
+//     {
+//         username: 'hellsent',
+//         email: 'hellsent@gmail.com'
+//     },
+//     {
+//         username: 'ayush',
+//         email: 'ayush@gmail.com'
+//     },
+//     {
+//         username: 'ayush1',
+//         email: 'ayush1@gmail.com'
+//     }
+// ];
+
+// class AppService {
+
+//     getUsers(req,res){
+//         return users;
+//     }
+
+//     createUser(user){
+//         users.push(user);
+//         return user;
+//     }
+
+//     getUsersByEmail(email){
+//         return users.filter(user => user.email === email);
+//     }
+
+//     removeUserByEmail(email){
+//         const data = users.filter(user => user.email !== email);
+//         users = data;
+//         return data; 
+//     }
+// }
+
+// importing user model
+const User = require('../database/models/user');
+
 
 class AppService {
 
     getUsers(req,res){
-        return users;
+        return User.find({});
     }
 
     createUser(user){
