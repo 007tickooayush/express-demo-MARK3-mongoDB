21e2633d5c504e4848c317f8dd665774702829b4
007tickooayush <ayush.tickoo292000@gmail.com>
2021-08-01 11:07:30 +0530

    deafult route GET response added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userRouter = require('./user');
 const cartRouter = require('./cart');
+const defaultRouter = require('./default');
 
 router.use('/user',userRouter);
 router.use('/cart',cartRouter);
-
+router.use('/',defaultRouter);
 module.exports = router;
